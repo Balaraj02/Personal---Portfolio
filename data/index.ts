5e37bfdd3f9bde1c89cbb47fd4efe69edc4dfc5f
Balaraj02 <bbalraj995@gmail.com>
2024-11-20 12:43:15 +0530

    first commit

---
+++ data/index.ts
@@ -0,0 +1,217 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/b1.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS Animation library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Real-Time Chat Application",
+    des: "Welcome to the Advanced Multi-User Real-Time Chat Application, designed for seamless, lag-free communication.",
+    img: "/p1.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
+    link: "/ui.earth.com",
+  },
+  {
+    id: 2,
+    title: "MI-Front-End Clone",
+    des: "Experience the best in online shopping with a sleek and intuitive interface, inspired by the MI front-end design-making product browsing and purchases effortless and enjoyable",
+    img: "/p2.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
+    link: "/ui.yoom.com",
+  },
+  {
+    id: 3,
+    title: "Shopify-Clone",
+    des: "A user-friendly interface designed with modern aesthetics in mind. Our platform simplifies product management, enabling effortless inventory tracking and collection optimization for improved discoverability.",
+    img: "/p3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "/ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Python Maze-Solver",
+    des: "The Maze Solver is an engaging game that uses depth-first search to navigate a grid-based maze from start to finish. It enhances problem-solving skills while providing an enjoyable coding experience in Python.",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    img: "/p4.svg",
+    link: "/ui.apple.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Balaraj was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Balaraj's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Balaraj is the ideal partner.",
+    name: "Chaitanya Betha",
+    title: "Software Developer at Razorpay",
+  },
+  {
+    quote:
+      "Collaborating with Balaraj was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Balaraj's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Balaraj is the ideal partner.",
+    name: "Chekuri Viroopaksh",
+    title: "Student at UC Sandiago",
+  },
+  {
+    quote:
+      "Collaborating with Balaraj was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Balaraj's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Balaraj is the ideal partner.",
+    name: "P Vijay Ramaraju",
+    title: "Software Developer at Honda-R&D",
+  },
+  {
+    quote:
+      "Collaborating with Balaraj was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Balaraj's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Balaraj is the ideal partner.",
+    name: "Manoj Kumar Saka",
+    title: "Research Associate in Neural Networks at IIT madras",
+  },
+  {
+    quote:
+      "Collaborating with Balaraj was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Balaraj's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Balaraj is the ideal partner.",
+    name: "Mohan Koduri",
+    title: "Software Developer at MindGate Technologies",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "cloudinary",
+    img: "/cloud.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/app.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/s.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "Frontend Engineer Intern",
+    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Mobile App Dev - Aatral ESP",
+    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Python Backend Developer",
+    desc: "Used Python to manage and optimize databases for storing and processing real-time data from general stores, ensuring efficient data handling and reliability in backend operations.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Multi - user chat-Application - Project",
+    desc: "Built using Node.js and Socket.io, enabling real-time communication and scalable messaging between users. The application supports multiple active users, ensuring efficient message handling, low-latency interactions, and seamless connectivity.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
